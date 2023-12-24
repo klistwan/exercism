1b889afd4d4157b017bb69a479c6b720a5cc4bc1
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2023-12-25 09:13:37 +1100

    Add clock solution

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,35 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+    }
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let mut total_minutes = hours * 60 + minutes;
+        while total_minutes < 0 {
+            total_minutes += 24 * 60;
+        }
+        Clock {
+            hours: (total_minutes / 60).rem_euclid(24),
+            minutes: total_minutes.rem_euclid(60),
+        }
+    }
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.hours == other.hours && self.minutes == other.minutes
+    }
+}

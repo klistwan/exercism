11e70b8b16ea9fbb5d72103a6a89e7d850bab654
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2023-12-30 16:30:35 +1100

    Refactor solution to sublist using match

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,23 +6,30 @@ pub enum Comparison {
     Unequal,
 }
 
-/// Returns boolean indicating if l1 has the subesquence l2.
+/// Returns boolean indicating if the smaller array is found in the biggest array.
 fn contains<T: PartialEq>(small: &[T], big: &[T]) -> bool {
     big.windows(small.len()).any(|window| window == small)
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list == _second_list {
-        return Comparison::Equal;
-    } else if _first_list.is_empty() {
-        return Comparison::Sublist;
-    } else if _second_list.is_empty() {
-        return Comparison::Superlist;
-    } else if _first_list.len() < _second_list.len() && contains(_first_list, _second_list) {
-        return Comparison::Sublist;
-    } else if _second_list.len() < _first_list.len() && contains(_second_list, _first_list) {
-        return Comparison::Superlist;
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Comparison::Equal,
+        (0, _) => Comparison::Sublist,
+        (_, 0) => Comparison::Superlist,
+        (a, b) if a < b => match contains(_first_list, _second_list) {
+            true => Comparison::Sublist,
+            false => Comparison::Unequal,
+        },
+        (a, b) if a > b => match contains(_second_list, _first_list) {
+            true => Comparison::Superlist,
+            false => Comparison::Unequal,
+        },
+        (_, _) => {
+            if _first_list == _second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
     }
-
-    Comparison::Unequal
 }
